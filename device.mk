d625edfc957ac79fa763d8249308d43fdd838b28
Steve Kondik <steve@cyngn.com>
2015-05-21 02:02:40 -0400

    oneplus2: Enable low latency output and capture * What Qualcomm was trying to fix was a real problem, it just created    even more problems which are incredibly difficult to solve such as    underruns and even broken apps (Loop reader, etc). Do it Google's    way and forget it- it's a non-issue with offloading anyway.

--- device.mk
+++ device.mk
@@ -60,6 +60,7 @@ PRODUCT_PACKAGES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml \
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
