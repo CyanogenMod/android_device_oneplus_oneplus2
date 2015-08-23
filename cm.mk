f13c11761ef8bec2551ceeb27bd49e449bbc453c
Willi Ye <williye97@gmail.com>
2015-08-23 23:43:01 +0200

    Call core_64_bit

--- cm.mk
+++ cm.mk
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from bacon device
