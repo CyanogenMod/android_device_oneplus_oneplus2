d76dc24ca52c02ffafa7d89ff39fe0b82abb9674
martincz <martincz@mokeedev.com>
2015-08-25 02:15:36 +0800

    plutonium: fix wrong config path
    Change-Id: If649b1bccbfb47350da483d706ea31b994630f15

--- Android.mk
+++ Android.mk
@@ -24,8 +24,8 @@ include $(CLEAR_VARS)
 
 # Create a link for the WCNSS config file, which ends up as a writable
 # version in /data/misc/wifi
-$(shell mkdir -p $(TARGET_OUT)/etc/firmware/wlan/qca_cld; \
-    ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
-        $(TARGET_OUT)/etc/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini)
+$(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/qca_cld; \
+    ln -sf /system/etc/wifi/WCNSS_qcom_cfg.ini \
+        $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini)
 
 endif
