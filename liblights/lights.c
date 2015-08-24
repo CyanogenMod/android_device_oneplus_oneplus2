52f694dc5dcf299938c485c89dc67e3c0b7f495f
Willi Ye <williye97@gmail.com>
2015-08-24 18:23:36 +0200

    liblights: More cleanup

--- liblights/lights.c
+++ liblights/lights.c
@@ -120,11 +120,9 @@ is_lit(struct light_state_t const *state)
 }
 
 static int
-set_speaker_light_locked_drv(struct light_device_t *dev,
+set_speaker_light_locked(struct light_device_t *dev,
         struct light_state_t const *state)
 {
-    int len;
-    int blink, freq, pwm;
     int onMS, offMS;
     unsigned int colorRGB;
 
@@ -138,6 +136,7 @@ set_speaker_light_locked_drv(struct light_device_t *dev,
 
     switch (state->flashMode) {
         case LIGHT_FLASH_TIMED:
+        case LIGHT_FLASH_HARDWARE:
             onMS = state->flashOnMS;
             offMS = state->flashOffMS;
             break;
@@ -149,43 +148,12 @@ set_speaker_light_locked_drv(struct light_device_t *dev,
     }
 
     colorRGB = state->color;
-
-    if (onMS > 0 && offMS > 0) {
-        int totalMS = onMS + offMS;
-
-        // the LED appears to blink about once per second if freq is 20
-        // 1000ms / 20 = 50
-        freq = totalMS / 50;
-        // pwm specifies the ratio of ON versus OFF
-        // pwm = 0 -> always off
-        // pwm = 255 => always on
-        pwm = (onMS * 255) / totalMS;
-
-        // the low 4 bits are ignored, so round up if necessary
-        if (pwm > 0 && pwm < 16)
-            pwm = 16;
-
-        blink = 1;
-    } else {
-        blink = 0;
-        freq = 0;
-        pwm = 0;
-    }
-
     write_int(RED_LED_FILE, (colorRGB >> 16) & 0xFF);
     write_int(GREEN_LED_FILE, (colorRGB >> 8) & 0xFF);
     write_int(BLUE_LED_FILE, colorRGB & 0xFF);
-
-    write_int(LED_BLINK_FILE, blink);
+    write_int(LED_BLINK_FILE, onMS > 0 && offMS > 0 ? 1 : 0);
 
     return 0;
-}
-
-static int
-set_speaker_light_locked(struct light_device_t *dev,
-        struct light_state_t const *state)
-{
-    return set_speaker_light_locked_drv(dev, state);
 }
 
 static void
@@ -304,39 +272,40 @@ close_lights(struct light_device_t *dev)
 static int open_lights(const struct hw_module_t *module, const char *name,
         struct hw_device_t **device)
 {
-    int (*set_light)(struct light_device_t *dev,
-            const struct light_state_t *state);
+    int (*set_light)(struct light_device_t* dev,
+            struct light_state_t const* state);
+    struct light_device_t *dev;
 
-    if (0 == strcmp(LIGHT_ID_BACKLIGHT, name))
-        set_light = set_light_backlight;
-    else if (0 == strcmp(LIGHT_ID_BUTTONS, name))
-        set_light = set_light_buttons;
-    else if (0 == strcmp(LIGHT_ID_NOTIFICATIONS, name))
-        set_light = set_light_notifications;
-    else if (0 == strcmp(LIGHT_ID_ATTENTION, name))
-        set_light = set_light_attention;
-    else if (0 == strcmp(LIGHT_ID_BATTERY, name))
-        set_light = set_light_battery;
-    else
-        return -EINVAL;
+    if (0 == strcmp(LIGHT_ID_BACKLIGHT, name)) {
+      set_light = set_light_backlight;
+    } else if (0 == strcmp(LIGHT_ID_BUTTONS, name)) {
+      set_light = set_light_buttons;
+    } else if (0 == strcmp(LIGHT_ID_BATTERY, name)) {
+      set_light = set_light_battery;
+    } else if (0 == strcmp(LIGHT_ID_ATTENTION, name)) {
+      set_light = set_light_attention;
+    } else if (0 == strcmp(LIGHT_ID_NOTIFICATIONS, name))  {
+      set_light = set_light_notifications;
+    } else {
+      return -EINVAL;
+    }
 
     pthread_once(&g_init, init_globals);
-
-    struct light_device_t *dev = malloc(sizeof(struct light_device_t));
-    memset(dev, 0, sizeof(*dev));
+    dev = malloc(sizeof(struct light_device_t));
+    memset(dev, 0, sizeof(struct light_device_t));
 
     dev->common.tag = HARDWARE_DEVICE_TAG;
     dev->common.version = 0;
-    dev->common.module = (struct hw_module_t*)module;
-    dev->common.close = (int (*)(struct hw_device_t*))close_lights;
+    dev->common.module = (struct hw_module_t*) module;
+    dev->common.close = (int (*)(struct hw_device_t*)) close_lights;
     dev->set_light = set_light;
 
-    *device = (struct hw_device_t*)dev;
+    *device = (struct hw_device_t*) dev;
     return 0;
 }
 
 static struct hw_module_methods_t lights_module_methods = {
-    .open =  open_lights,
+    .open = open_lights,
 };
 
 /*
@@ -347,7 +316,7 @@ struct hw_module_t HAL_MODULE_INFO_SYM = {
     .version_major = 1,
     .version_minor = 0,
     .id = LIGHTS_HARDWARE_MODULE_ID,
-    .name = "Plutonium Lights Module",
+    .name = "Lights module",
     .author = "The CyanogenMod Project",
     .methods = &lights_module_methods,
 };
