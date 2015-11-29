818ca719eb90db96257c438bb29556003ff2a017
Michael Bestas <mikeioannina@gmail.com>
2015-11-29 15:33:45 +0200

    Use ScreenType util from cmsdk
    Change-Id: I741eb0807062e5074354fe1ecbf9831a2d1ea66c

--- configpanel/Android.mk
+++ configpanel/Android.mk
@@ -23,5 +23,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_CERTIFICATE := platform
 LOCAL_PACKAGE_NAME := ConfigPanel
 
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    org.cyanogenmod.platform.internal
+
 include $(BUILD_PACKAGE)
 endif
--- configpanel/src/com/cyanogenmod/settings/device/TouchscreenGestureSettings.java
+++ configpanel/src/com/cyanogenmod/settings/device/TouchscreenGestureSettings.java
@@ -16,13 +16,13 @@
 
 package com.cyanogenmod.settings.device;
 
-import com.android.internal.util.cm.ScreenType;
-
 import java.io.File;
 
 import com.cyanogenmod.settings.device.utils.Constants;
 import com.cyanogenmod.settings.device.utils.FileUtils;
 import com.cyanogenmod.settings.device.utils.NodePreferenceActivity;
+
+import org.cyanogenmod.internal.util.ScreenType;
 
 import android.os.Bundle;
 import android.preference.ListPreference;
