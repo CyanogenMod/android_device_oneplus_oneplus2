18de077da7e544af36c269b839b16bda218f51b5
Danny Baumann <dannybaumann@web.de>
2015-11-30 09:07:22 +0100

    Track ril header changes.
    Change-Id: I69503f31b89c45b29712d2de8a1ecb09b79d62c7

--- include/telephony/ril.h
+++ include/telephony/ril.h
@@ -4528,6 +4528,26 @@ typedef struct {
  */
 #define RIL_REQUEST_GET_ACTIVITY_INFO 135
 
+/**
+ * RIL_REQUEST_SIM_GET_ATR
+ *
+ * Get the ATR from SIM Card
+ *
+ * Only valid when radio state is "RADIO_STATE_ON"
+ *
+ * "data" is const int *
+ * ((const int *)data)[0] contains the slot index on the SIM from which ATR is requested.
+ *
+ * "response" is a const char * containing the ATR, See ETSI 102.221 8.1 and ISO/IEC 7816 3
+ *
+ * Valid errors:
+ *
+ * SUCCESS
+ * RADIO_NOT_AVAILABLE (radio resetting)
+ * GENERIC_FAILURE
+ */
+#define RIL_REQUEST_SIM_GET_ATR 136
+
 /***********************************************************************/
 
 
