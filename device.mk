391bdad81f0d6221212489a5e4b81362f802e137
Neil Agarwal <regalstreak@gmail.com>
2015-12-01 21:34:07 +0530

    OnePlus2: Build libcnefeatureconfig from source

--- device.mk
+++ device.mk
@@ -54,6 +54,10 @@ PRODUCT_PACKAGES += \
     init.qcom.ril.sh \
     init.qcom.uicc.sh
 
+# RIL
+PRODUCT_PACKAGES += \
+    libcnefeatureconfig
+
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml \
