3fe6db4c3aa159b9db5de12bd23b0cde42bb078f
Abhisek Devkota <ciwrl@cyanogenmod.com>
2015-11-30 16:42:11 -0800

    WIP Sepolicy
    Change-Id: I67cdf1bf6deef02b972238b7acc2f900f81d29c1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -177,6 +177,34 @@ include device/qcom/sepolicy/sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += device/oneplus/oneplus2/sepolicy
 
+BOARD_SEPOLICY_UNION += \
+    adspd.te \
+    atfwd.te \
+    bluetooth_loader.te \
+    bridge.te \
+    camera.te \
+    device.te \
+    file.te \
+    gsiffd.te \
+    healthd.te \
+    irsc_util.te \
+    mdm_helper.te \
+    mediaserver.te \
+    mpdecision.te \
+    mm-qcamerad.te \
+    qseecomd.te \
+    rild.te \
+    rmt_storage.te \
+    ueventd.te \
+    sensors.te \
+    surfaceflinger.te \
+    system_app.te \
+    system_server.te \
+    wcnss_service.te \
+    file_contexts \
+    genfs_contexts \
+    property_contexts
+
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
 
