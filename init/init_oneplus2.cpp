54be8db694741f092356c111d654b7e67bdb24f5
Roman Birg <roman@cyngn.com>
2015-12-03 18:05:06 -0800

    oneplus2: Sim2 can be LTE too
    Change-Id: Ia6c28382067e19e3a07ca1cc1f774c76d52b346a
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- init/init_oneplus2.cpp
+++ init/init_oneplus2.cpp
@@ -56,18 +56,18 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.product.model", "ONE A2001");
         property_set("ro.rf_version", "TDD_FDD_Ch_All");
         property_set("telephony.lteOnCdmaDevice", "1");
-        property_set("ro.telephony.default_network", "20");
+        property_set("ro.telephony.default_network", "20,20");
     } else if (strstr(rf_version, "24")) {
         /* Asia/Europe */
         property_set("ro.product.model", "ONE A2003");
         property_set("ro.rf_version", "TDD_FDD_Eu");
-        property_set("ro.telephony.default_network", "9");
+        property_set("ro.telephony.default_network", "9,9");
     } else if (strstr(rf_version, "34")) {
         /* America */
         property_set("ro.product.model", "ONE A2005");
         property_set("ro.rf_version", "TDD_FDD_Am");
         property_set("telephony.lteOnCdmaDevice", "1");
-        property_set("ro.telephony.default_network", "9");
+        property_set("ro.telephony.default_network", "9,9");
     }
 }
 
