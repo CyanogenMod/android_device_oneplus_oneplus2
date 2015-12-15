a96492b20fbb74a501f77e76898ffbc69cc49f53
Neil Agarwal <regalstreak@gmail.com>
2015-12-15 22:19:14 +0530

    FP: Bring up fingerprint sensor for M

--- device.mk
+++ device.mk
@@ -71,6 +71,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
     frameworks/native/data/etc/android.hardware.hdmi.cec.xml:system/etc/permissions/android.hardware.hdmi.cec.xml \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
@@ -148,8 +149,9 @@ PRODUCT_PACKAGES += \
     setup_fs
 
 # Fingerprint sensor
-#PRODUCT_PACKAGES += \
-#    fingerprint.msm8994
+PRODUCT_PACKAGES += \
+     fingerprintd
+#    fingerprint.msm8994 \
 
 # GPS
 PRODUCT_PACKAGES += \
