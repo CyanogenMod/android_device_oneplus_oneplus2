CyanogenMod/android_device_oppo_r7plus

Update fingerprint HAL for M.
Change-Id: Ib14f71c8bec26c0f763a8e22598db84b1331aeeb

device.mk
@@ -58,9 +58,10 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint sensor
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/cyanogenmod.hardware.fingerprint.xml:system/etc/permissions/cyanogenmod.hardware.fingerprint.xml
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
 PRODUCT_PACKAGES += \
-    fingerprint.msm8916
+    fingerprint.msm8916 \
+    fingerprintd
 
 # Input configuration
 PRODUCT_COPY_FILES += \

CyanogenMod/android_device_oneplus_oneplus2

device.mk
@@ -63,6 +63,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.full.xml:system/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:system/etc/permissions/android.hardware.camera.raw.xml \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
     frameworks/native/data/etc/android.hardware.hdmi.cec.xml:system/etc/permissions/android.hardware.hdmi.cec.xml \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml \
@@ -149,8 +150,9 @@ PRODUCT_PACKAGES += \
     setup_fs
 
 # Fingerprint sensor
-#PRODUCT_PACKAGES += \
-#    fingerprint.msm8994
+PRODUCT_PACKAGES += \
+    fingerprint.msm8994 \
+    fingerprintd
 
 # GPS
 PRODUCT_PACKAGES += \