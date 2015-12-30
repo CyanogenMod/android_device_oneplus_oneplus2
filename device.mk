b0d583d9dfa661a8b5dcb7f57cfd2388de478ad3
Neil Agarwal <regalstreak@gmail.com>
2015-12-30 15:24:59 +0100

    device.mk: Fix typo WTF how did a + char some there :3

--- device.mk
+++ device.mk
@@ -204,7 +204,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
 
-+# OMX
+# OMX
  PRODUCT_PACKAGES += \
      libc2dcolorconvert \
      libdashplayer \
