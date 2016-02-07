b09ee6be41f09fe2bf66b30aaecaed3205dda69d
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2016-02-07 23:23:46 +0000

    msm8994: add conf file to enable sensors multi hal feature
    Sensors multi hal reads the conf file and loads the sensor
    hal libraries mentioned to support sensors multi hal feature.
    
    Change-Id: I91df2187351c291b412bccc4026fdfecb5e6a8d3

--- device.mk
+++ device.mk
@@ -239,6 +239,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:system/etc/sensors/hals.conf \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf
 
 # Thermal config
