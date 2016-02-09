fd57de065a2612839b0d2ab00bdf65df07892e16
Willi Ye <williye97@gmail.com>
2016-02-09 19:40:21 +0100

    Remove libboringssl-compat and libstlport

--- device.mk
+++ device.mk
@@ -76,11 +76,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
-# Adreno driver dependencies
-PRODUCT_PACKAGES += \
-    libboringssl-compat \
-    libstlport
-
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
@@ -121,7 +116,6 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    libstlport \
     Snap
 
 # Charger
