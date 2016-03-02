e2c9c91add2feb90dcf0be88da569e52a8023ab9
Pierre Imai <imaipi@google.com>
2016-03-02 17:47:29 +0900

    oneplus2: Remove references to deleted dhcpcd.conf
    Change-Id: I7221f05d98ea7bfbdd3c935302fd7d68b99a59bb

--- device.mk
+++ device.mk
@@ -277,7 +277,6 @@ PRODUCT_PACKAGES += \
     libQWiFiSoftApCfg \
     libwpa_client \
     hostapd \
-    dhcpcd.conf \
     wpa_supplicant \
     wpa_supplicant.conf \
     wpa_supplicant_overlay.conf \
