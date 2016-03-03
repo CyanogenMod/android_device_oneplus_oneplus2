8a83e400581619c7c9b1d9d110348c29f383e86d
Keith Mok <kmok@cyngn.com>
2016-03-03 15:32:08 -0800

    oneplus2: Completely remove QC location stack
    * liblbs_core checks for existence of com.qualcomm.location to determine
      whether location injection occurs.
    
    CYNGNOS-1514
    
    Change-Id: I7ee8987789efd2e7741963b7fff3877a9c0f569d

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -61,9 +61,6 @@ PRODUCT_PACKAGES += \\
     dpmapi
 
 PRODUCT_PACKAGES += \\
-    com.qualcomm.location
-
-PRODUCT_PACKAGES += \\
     qcrilmsgtunnel \\
     shutdownlistener \\
     TimeService
@@ -141,17 +138,6 @@ include \$(CLEAR_VARS)
 LOCAL_MODULE := CNEService
 LOCAL_MODULE_OWNER := $VENDOR
 LOCAL_SRC_FILES := proprietary/priv-app/CNEService/CNEService.apk
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE_CLASS := APPS
-LOCAL_PRIVILEGED_MODULE := true
-LOCAL_CERTIFICATE := platform
-include \$(BUILD_PREBUILT)
-
-include \$(CLEAR_VARS)
-LOCAL_MODULE := com.qualcomm.location
-LOCAL_MODULE_OWNER := $VENDOR
-LOCAL_SRC_FILES := proprietary/priv-app/com.qualcomm.location/com.qualcomm.location.apk
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MODULE_CLASS := APPS
