3e81add76d27c8b140c5132228842d2babf98296
thecrazyskull <anaskarbila@aospa.co>
2016-04-01 10:22:48 -0400

    oneplus2: enable dexpreopt for userdebug
    * call match-word-in-list doesn't seem to work
    
    Change-Id: Ibe532e0dcb00c8ba4a5b48d12524e0989d3e8d78

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -152,7 +152,7 @@ TARGET_NO_RPC := true
 
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
-  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
     endif
