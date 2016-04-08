4960bc4592637e34728339e66860ddeeeaff69ab
Willi Ye <williye97@gmail.com>
2016-04-08 18:38:39 +0200

    Move qcrilmsgtunnel to priv-app
    Change-Id: I9f91a020cf95821b188b3bef541f45d101ec818f

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -148,10 +148,11 @@ include \$(BUILD_PREBUILT)
 include \$(CLEAR_VARS)
 LOCAL_MODULE := qcrilmsgtunnel
 LOCAL_MODULE_OWNER := $VENDOR
-LOCAL_SRC_FILES := proprietary/app/qcrilmsgtunnel/qcrilmsgtunnel.apk
+LOCAL_SRC_FILES := proprietary/priv-app/qcrilmsgtunnel/qcrilmsgtunnel.apk
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
 LOCAL_CERTIFICATE := platform
 include \$(BUILD_PREBUILT)
 
