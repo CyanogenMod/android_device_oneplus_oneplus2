196bc5179b8eeb6640ec6da0b48ca277ee6a0007
Willi Ye <williye97@gmail.com>
2016-04-08 18:21:22 +0200

    Update configs
    * OnePlus2Hydrogen_14_1604282132
    * Add missing audio output policy
    
    Change-Id: I51187814210f7bdd952610b48d8a964e39ccc691

--- device.mk
+++ device.mk
@@ -88,6 +88,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/acdb/MTP_Speaker_cal.acdb:system/etc/acdbdata/MTP/MTP_Speaker_cal.acdb \
     $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/listen_platform_info.xml:system/etc/listen_platform_info.xml \
@@ -142,7 +143,6 @@ PRODUCT_PACKAGES += \
     gps.conf \
     izat.conf \
     lowi.conf \
-    msap.conf \
     quipc.conf \
     sap.conf \
     xtwifi.conf
