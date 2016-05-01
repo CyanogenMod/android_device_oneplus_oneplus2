910f48406e5426600c87d85388456faedd3aa1f3
Steve Kondik <steve@cyngn.com>
2016-04-30 21:40:08 -0700

    oneplus2: Build with Clang
    * Boldly going where no-one has gone before.
    
    * 10mb smaller rom.zip size
    * 40 minutes less build time
    
    Change-Id: I7527f4fa8030a6cde3c37f8148b2a4afb73d0908

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,6 +59,9 @@ TARGET_CPU_CORTEX_A53 := true
 TARGET_BOARD_SUFFIX := _64
 TARGET_USES_64_BIT_BINDER := true
 
+# Clang
+USE_CLANG_PLATFORM_BUILD := true
+
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5
 BOARD_KERNEL_BASE := 0x00000000
