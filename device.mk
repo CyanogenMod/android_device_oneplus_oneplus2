f62f7301a5429fd1b9872d8e4a2b8a41782b9b90
Willi Ye <williye97@gmail.com>
2016-06-03 20:21:58 +0200

    Use dalvik heap/hwui for 4gb phones
    Change-Id: I52ebebcd0a2b4d52fb270f2eb90fb4ee79140e1f

--- device.mk
+++ device.mk
@@ -58,8 +58,8 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-$(call inherit-product, frameworks/native/build/phone-xxxhdpi-3072-dalvik-heap.mk)
-$(call inherit-product, frameworks/native/build/phone-xxxhdpi-3072-hwui-memory.mk)
+$(call inherit-product, frameworks/native/build/phone-xxxhdpi-4096-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xxxhdpi-4096-hwui-memory.mk)
 
 # Haters gonna hate..
 PRODUCT_CHARACTERISTICS := nosdcard
