9f846f1dc71a8befe450a9b83c0ebb99e7fe87f5
c_rrajiv <rrajiv@codeaurora.org>
2016-08-30 14:06:31 +0530

    msm8994: Add NET_BIND_SERVICE for cnss-daemon
    Add NET_BIND_SERVICE for cnss-daemon as it is required for QMI
    conmunications.
    Change-Id: I924c5dd3100a72c2af55008b1493f47b9e59e191
    CRs-Fixed: 1048891

--- android_filesystem_config.h
+++ android_filesystem_config.h
@@ -34,5 +34,6 @@
       // { 00755, AID_UID,     AID_GID,     (1ULL << CAPABILITY), "PATH_TO_BINARY" },
       { 00755, AID_SYSTEM,      AID_SYSTEM,      (1ULL << CAP_NET_BIND_SERVICE), "system/bin/pm-service"},
       { 00755, AID_SYSTEM,         AID_SYSTEM,         (1ULL << CAP_NET_BIND_SERVICE), "system/bin/imsdatadaemon" },
+      { 00755, AID_SYSTEM,         AID_SYSTEM,         (1ULL << CAP_NET_BIND_SERVICE), "system/bin/cnss-daemon"},
       { 00755, AID_SYSTEM,         AID_RADIO,          (1ULL << CAP_NET_BIND_SERVICE), "system/bin/ims_rtp_daemon" },
 };
