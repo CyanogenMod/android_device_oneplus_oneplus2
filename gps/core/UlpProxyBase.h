d754d246afa79741af566986b7afb2aff9456f51
Willi Ye <williye97@gmail.com>
2016-09-07 16:28:00 +0200

    gps: Rename QcomGnssSvStatus for hexing
    Change-Id: I3862b2fcee99b6de1d5ad12f7a6de3077c3a92bb

--- gps/core/UlpProxyBase.h
+++ gps/core/UlpProxyBase.h
@@ -59,7 +59,7 @@ public:
                                        LocPosTechMask loc_technology_mask) {
         return false;
     }
-    inline virtual bool reportSv(QcomGnssSvStatus &svStatus,
+    inline virtual bool reportSv(QcomSvStatus &svStatus,
                                  GpsLocationExtended &locationExtended,
                                  void* svExt) {
         return false;
