e04bd56604d34ab512a350c94c1ba0601ef5bf10
Willi Ye <williye97@gmail.com>
2016-09-14 03:01:59 +0200

    wifi: Remove nv bin
    * Not needed
    
    Change-Id: I8e1f59178ca8d455a56be651acaed4df81b938b5

--- device.mk
+++ device.mk
@@ -262,8 +262,7 @@ PRODUCT_COPY_FILES += \
 # WiFi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
-    $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/qca_cld/WCNSS_cfg.dat \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/qca_cld/WCNSS_qcom_wlan_nv.bin
+    $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/qca_cld/WCNSS_cfg.dat
 
 PRODUCT_PACKAGES += \
     ipacm \
