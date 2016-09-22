8078e5f7a36599dff201b3ffeedb4a1d671e0529
Willi Ye <williye97@gmail.com>
2016-09-22 14:34:42 +0200

    Build livedisplay native
    Change-Id: I282367287b7088ad22a7cb4253cb9261a8d497a7

--- device.mk
+++ device.mk
@@ -176,6 +176,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     lights.msm8994
 
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+    libjni_livedisplay
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
