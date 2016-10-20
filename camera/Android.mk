dc4563841363630ec320c5b22476a77315359a63
Willi Ye <williye97@gmail.com>
2016-10-20 20:02:00 +0200

    camera: Shim sensor list
    Change-Id: I1c96076d477c98c60f7f1cfdfb71cc47dd3a3982

--- camera/Android.mk
+++ camera/Android.mk
@@ -59,4 +59,15 @@ LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
 
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := camera_shim.cpp
+LOCAL_MODULE := libcamera_shim
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_32_BIT_ONLY := true
+LOCAL_SHARED_LIBRARIES := libgui
+
+include $(BUILD_SHARED_LIBRARY)
+
 endif # !TARGET_SIMULATOR
