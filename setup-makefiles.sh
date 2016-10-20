6a1e5215c8b590090a12ea39347dc790792f1747
Willi Ye <williye97@gmail.com>
2016-10-20 15:53:56 +0200

    Use shared graphic blobs
    Change-Id: I7dd4c5d5cffb4206232cf9fe8556646a20fb0616

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -45,9 +45,9 @@ write_makefiles "$MY_DIR"/proprietary-files.txt
 
 cat << EOF >> "$ANDROIDMK"
 
-\$(shell mkdir -p \$(PRODUCT_OUT)/system/vendor/lib/egl && pushd \$(PRODUCT_OUT)/system/vendor/lib > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
-\$(shell mkdir -p \$(PRODUCT_OUT)/system/vendor/lib64/egl && pushd \$(PRODUCT_OUT)/system/vendor/lib64 > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
 EOF
+
+printf '\n%s\n' "\$(call inherit-product, vendor/qcom/binaries/msm8994/graphics/graphics-vendor.mk)" >> "$PRODUCTMK"
 
 # We are done!
 write_footers
