4bd531583845ddde7c0c2af2cc6fd19372d4c94a
Willi Ye <williye97@gmail.com>
2016-10-27 16:50:13 +0200

    camera: Switch to OSS hal
    Change-Id: I7fb8791d5b5312f8562f7c3e61316694c4365236

---
+++ camera/common.mk
@@ -0,0 +1,5 @@
+common_deps :=
+kernel_includes :=
+
+common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
--- device.mk
+++ device.mk
@@ -102,7 +102,8 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8994 \
-    libcamera_shim \
+    mm-qcamera-app \
+    libmm-qcamera \
     sensors.hal.tof \
     Snap
 
