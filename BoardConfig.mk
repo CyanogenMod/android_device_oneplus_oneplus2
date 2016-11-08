ee804ca83377eaf4333ab4c7c0e468d0b6df2e04
Steve Kondik <steve@cyngn.com>
2016-11-08 12:54:58 -0800

    oneplus2: Build with SDCLANG
    Change-Id: I76b21bd40ea0d4b29dec98e75ebf13ebe479752a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,6 +29,9 @@ PLATFORM_PATH := device/oneplus/oneplus2
 
 BOARD_VENDOR := oneplus
 
+# Use Snapdragon LLVM, if available
+TARGET_USE_SDCLANG := true
+
 # Assertions
 TARGET_BOARD_INFO_FILE ?= $(PLATFORM_PATH)/board-info.txt
 
