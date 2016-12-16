49e1ff542393740d8b6cb50689aa39f97da8c8f6
kunal <kunalshah912@gmail.com>
2016-12-16 15:40:05 +0530

    nitrogen changes

--- nitrogen_oneplus2.mk
+++ nitrogen_oneplus2.mk
@@ -17,12 +17,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from oneplus2 device
-$(call inherit-product, device/oneplus/oneplus2/device.mk)
+$(call inherit-product, device/oneplus/oneplus2/oneplus2.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/nitrogen/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_oneplus2
+PRODUCT_NAME := nitrogen_oneplus2
 PRODUCT_DEVICE := oneplus2
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
